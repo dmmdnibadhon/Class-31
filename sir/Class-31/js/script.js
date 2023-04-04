a5a8a9964df290a65612db24c8906d8f020db67c
dmmdnibadhon <mdnazmulislambadhon@gmail.com>
2023-04-04 01:59:47 -0700

    Class-31

---
+++ sir/Class-31/js/script.js
@@ -0,0 +1,14 @@
+$('.dest_slide').slick({
+    slidesToShow: 5,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    arrows: false,
+    centerMode: true,
+    centerPadding: 0,
+  });
+
+  $('.counter').counterUp({
+    delay: 10,
+    time: 1000
+});
